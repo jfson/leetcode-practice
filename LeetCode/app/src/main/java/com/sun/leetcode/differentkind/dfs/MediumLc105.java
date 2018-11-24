3625cc49378d4c244db8e510ec4338e9be1bbf5a
jfson <sunzhenandroid@126.com>
2018-11-24 17:02:05 +0800

    lc 105

---
+++ LeetCode/app/src/main/java/com/sun/leetcode/differentkind/dfs/MediumLc105.java
@@ -0,0 +1,38 @@
+package com.example.lctest.dfs;
+
+import com.example.lctest.model.TreeNode;
+
+/**
+ * Author: sunzhen
+ * Create on:  2018/11/21
+ * Description:
+ */
+public class MediumLc105 {
+
+    /**
+     105. Construct Binary Tree from Preorder and Inorder Traversal
+     Given preorder and inorder traversal of a tree, construct the binary tree.
+
+     Note:
+     You may assume that duplicates do not exist in the tree.
+
+     For example, given
+
+     preorder = [3,9,20,15,7]
+     inorder = [9,3,15,20,7]
+     Return the following binary tree:
+
+        3
+      /  \
+     9   20
+     /   \
+     15   7
+     题意：给定前序遍历和后序遍历值，复原二叉树。Note:无重复项
+
+     */
+
+    public TreeNode buildTree(int[] preorder, int[] inorder) {
+        return null;
+    }
+
+}
