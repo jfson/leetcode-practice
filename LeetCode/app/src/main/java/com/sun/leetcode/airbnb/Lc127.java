f29bddf254d1a2d7cf7e2f5030303f9ec7f1dd25
jfson <sunzhenandroid@126.com>
2018-12-12 22:56:03 +0800

    1.lc 127

--- LeetCode/app/src/main/java/com/sun/leetcode/airbnb/Lc127.java
+++ LeetCode/app/src/main/java/com/sun/leetcode/airbnb/Lc127.java
@@ -52,35 +52,35 @@ public class Lc127 {
      * solution:最短路径 BFS.改进：双向BFS
      */
     public int ladderLength(String beginWord, String endWord, List<String> wordList) {
-        Set<String> beginSet = new HashSet<String>(), endSet = new HashSet<String>();
-
-        int len = 1;
-        int strLen = beginWord.length();
-        HashSet<String> visited = new HashSet<String>();
+        if (!wordList.contains(endWord)) {
+            return 0;
+        }
+        Set<String> dict = new HashSet<>(wordList);
+        Set<String> beginSet = new HashSet<>();
+        Set<String> endSet = new HashSet<>();
         beginSet.add(beginWord);
         endSet.add(endWord);
 
+        int step = 1;
+        Set<String> visited = new HashSet<>();
         while (!beginSet.isEmpty() && !endSet.isEmpty()) {
-            if (beginSet.size() > endSet.size()) { //swap
+            if (beginSet.size() > endSet.size()) {
                 Set<String> set = beginSet;
                 beginSet = endSet;
                 endSet = set;
             }
-
             Set<String> temp = new HashSet<>();
             for (String word : beginSet) {
                 char[] chs = word.toCharArray();
                 for (int i = 0; i < chs.length; i++) {
-                    for (char j = 'a'; j < 'z'; j++) {
+                    for (char c = 'a'; c <= 'z'; c++) {
                         char old = chs[i];
-                        chs[i] = j;
+                        chs[i] = c;
                         String target = String.valueOf(chs);
-
                         if (endSet.contains(target)) {
-                            return len + 1;
+                            return step + 1;
                         }
-
-                        if (!visited.contains(target) && wordList.contains(target)) {
+                        if (!visited.contains(target) && dict.contains(target)) {
                             temp.add(target);
                             visited.add(target);
                         }
@@ -89,7 +89,7 @@ public class Lc127 {
                 }
             }
             beginSet = temp;
-            len++;
+            step++;
         }
         return 0;
     }
