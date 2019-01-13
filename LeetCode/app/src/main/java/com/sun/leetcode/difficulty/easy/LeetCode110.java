4e27ba8607d547e4b3632ea1cf10abc1973b1c55
jfson <sunzhenandroid@126.com>
2019-01-13 21:01:33 +0800

    1.Lc 110 & 38

--- LeetCode/app/src/main/java/com/sun/leetcode/difficulty/easy/LeetCode110.java
+++ LeetCode/app/src/main/java/com/sun/leetcode/difficulty/easy/LeetCode110.java
@@ -12,7 +12,42 @@ import com.sun.leetcode.model.TreeNode;
  * Train of thought:
  */
 public class LeetCode110 {
-    // 根据deep,左右树层级不超过1
+    /**
+     110. Balanced Binary Tree
+     Easy
+
+     Given a binary tree, determine if it is height-balanced.
+
+     For this problem, a height-balanced binary tree is defined as:
+
+     a binary tree in which the depth of the two subtrees of every node never differ by more than 1.
+
+     Example 1:
+
+     Given the following tree [3,9,20,null,null,15,7]:
+
+     3
+     / \
+     9  20
+     /  \
+     15   7
+     Return true.
+
+     Example 2:
+
+     Given the following tree [1,2,2,3,3,null,null,4,4]:
+
+     1
+     / \
+     2   2
+     / \
+     3   3
+     / \
+     4   4
+
+     题意：判断是不是高度平衡二叉树
+     solution: 根据deep,左右树层级不超过1
+     */
     public boolean isBalanced(TreeNode root) {
         if (checkBalanced(root) == -1) {
             return false;
@@ -33,6 +68,7 @@ public class LeetCode110 {
         if (Math.abs(left - right) > 1) {
             return -1;
         } else {
+            // 返回其最大深度
             return Math.max(left, right) + 1;
         }
     }
--- LeetCode/app/src/main/java/com/sun/leetcode/difficulty/easy/LeetCode38.java
+++ LeetCode/app/src/main/java/com/sun/leetcode/difficulty/easy/LeetCode38.java
@@ -8,13 +8,38 @@ package com.sun.leetcode.difficulty.easy;
  * Train of thought:
  */
 public class LeetCode38 {
-//    1.     1
-//    2.     11
-//    3.     21
-//    4.     1211
-//    5.     111221
 
-    // 数前一项的 1的个数，2的个数
+    /**
+     38. Count and Say
+     Easy
+
+     The count-and-say sequence is the sequence of integers with the first five terms as following:
+
+     1.     1
+     2.     11
+     3.     21
+     4.     1211
+     5.     111221
+     1 is read off as "one 1" or 11.
+     11 is read off as "two 1s" or 21.
+     21 is read off as "one 2, then one 1" or 1211.
+
+     Given an integer n where 1 ≤ n ≤ 30, generate the nth term of the count-and-say sequence.
+
+     Note: Each term of the sequence of integers will be represented as a string.
+
+
+
+     Example 1:
+
+     Input: 1
+     Output: "1"
+     Example 2:
+
+     Input: 4
+     Output: "1211"
+     题意：数前一项的 1的个数，2的个数
+     */
     public String countAndSay(int n) {
         int i = 1;
         String res = "1";
