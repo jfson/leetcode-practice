77d569c08521479674e511d59d63260a21d6a5d9
jfson <sunzhenandroid@126.com>
2019-01-13 17:57:23 +0800

    1.Lc 107

--- LeetCode/app/src/main/java/com/sun/leetcode/difficulty/easy/LeetCode107.java
+++ LeetCode/app/src/main/java/com/sun/leetcode/difficulty/easy/LeetCode107.java
@@ -12,6 +12,8 @@ import java.util.List;
  * Question:    Binary Tree Level Order Traversal II
  * Description: Given a binary tree, return the bottom-up level order traversal of its nodes' values. (ie, from left to right, level by level from leaf to root).
  * Train of thought:
+ * 题意：遍历二叉树
+ soulution: 利用LinkedList.addFirst()和递归进行解
  */
 public class LeetCode107 {
 
