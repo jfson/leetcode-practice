35589f7bdda303f69cd32d502ec46b4bf37fa064
jfson <sunzhenandroid@126.com>
2019-01-14 21:56:23 +0800

    1.905

--- LeetCode/app/src/main/java/com/sun/leetcode/differentkind/array/EasyLc905.java
+++ LeetCode/app/src/main/java/com/sun/leetcode/differentkind/array/EasyLc905.java
@@ -1,9 +1,5 @@
 package com.sun.leetcode.differentkind.array;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-
 /**
  * Author: jfson sun
  * Create on:  2018/10/29
@@ -12,7 +8,30 @@ import java.util.List;
  * Train of thought:
  */
 public class EasyLc905 {
+    /**
+     905. Sort Array By Parity
+     Easy
 
+     Given an array A of non-negative integers, return an array consisting of all the even elements of A, followed by all the odd elements of A.
+
+     You may return any answer array that satisfies this condition.
+
+
+
+     Example 1:
+
+     Input: [3,1,2,4]
+     Output: [2,4,3,1]
+     The outputs [4,2,3,1], [2,4,1,3], and [4,2,1,3] would also be accepted.
+
+
+     Note:
+
+     1 <= A.length <= 5000
+     0 <= A[i] <= 5000
+     题意：排序，偶数排在前面
+     solution: 依次交换偶数到前置索引
+     */
 
     //solution 1
     public int[] sortArrayByParity(int[] A) {
