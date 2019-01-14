2cb06e40fb30cecb24edc7a8223bbb32580feee8
jfson <sunzhenandroid@126.com>
2019-01-14 22:21:10 +0800

    1.561

--- LeetCode/app/src/main/java/com/sun/leetcode/differentkind/array/EasyLc561.java
+++ LeetCode/app/src/main/java/com/sun/leetcode/differentkind/array/EasyLc561.java
@@ -10,10 +10,26 @@ import java.util.Arrays;
  * Train of thought:
  */
 public class EasyLc561 {
+    /**
+     561. Array Partition I
+
+     Given an array of 2n integers, your task is to group these integers into n pairs of integer, say (a1, b1), (a2, b2), ..., (an, bn) which makes sum of min(ai, bi) for all i from 1 to n as large as possible.
+
+     Example 1:
+     Input: [1,4,3,2]
+
+     Output: 4
+     Explanation: n is 2, and the maximum sum of pairs is 4 = min(1, 2) + min(3, 4).
+     Note:
+     n is a positive integer, which is in the range of [1, 10000].
+     All the integers in the array will be in the range of [-10000, 10000].
+     题意：排序拆分
+
+      先sort
+     每2个取i值即可
+     */
 
 
-    // 先sort
-    // 每2个取i值即可
     public int arrayPairSum(int[] nums) {
         if(nums == null || nums.length == 0) return 0;
         Arrays.sort(nums);
@@ -24,5 +40,4 @@ public class EasyLc561 {
 
         return ret;
     }
-
 }
