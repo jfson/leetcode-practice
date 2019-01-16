4da01abd09e49ae54bc76c3f926a7d32260f88ed
jfson <sunzhenandroid@126.com>
2019-01-16 22:11:15 +0800

    1.hashtable 136

--- LeetCode/app/src/main/java/com/sun/leetcode/difficulty/easy/LeetCode136.java
+++ LeetCode/app/src/main/java/com/sun/leetcode/difficulty/easy/LeetCode136.java
@@ -11,6 +11,26 @@ import java.util.Arrays;
  * Train of thought:
  */
 public class LeetCode136 {
+    /**
+     136. Single Number
+     Easy
+
+     Given a non-empty array of integers, every element appears twice except for one. Find that single one.
+
+     Note:
+
+     Your algorithm should have a linear runtime complexity. Could you implement it without using extra memory?
+
+     Example 1:
+
+     Input: [2,2,1]
+     Output: 1
+     Example 2:
+
+     Input: [4,1,2,1,2]
+     Output: 4
+     找出落单的那个
+     */
     public int singleNumber(int[] nums) {
         if (nums == null || nums.length == 0) return 0;
 
