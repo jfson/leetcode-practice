b2567b8b98f0a2b25b935d74ddf2f4d4d6290eee
jfson <sunzhenandroid@126.com>
2019-03-19 23:47:21 +0800

    1.ms572 fix

--- LeetCode/app/src/main/java/com/sun/leetcode/company/Ms/Lc572.java
+++ LeetCode/app/src/main/java/com/sun/leetcode/company/Ms/Lc572.java
@@ -50,21 +50,17 @@ public class Lc572 {
      */
 
     public boolean isSubtree(TreeNode s, TreeNode t) {
-
-        if(s == null) return false;
-        if(isSame(s,t)) return true;
-        return isSame(s.left,t) || isSame(s.right,t);
+        if (s == null) return false;
+        if (isSame(s, t)) return true;
+        return isSubtree(s.left, t) || isSubtree(s.right, t);
     }
 
-    private boolean isSame(TreeNode s, TreeNode t){
-        if(s == null && t == null) return true;
+    private boolean isSame(TreeNode s, TreeNode t) {
+        if (s == null && t == null) return true;
+        if (s == null || t == null) return false;
 
-        if(s == null || t == null) return false;
+        if (s.val != t.val) return false;
 
-        if(s.val != t.val){
-            return false;
-        }
-
-        return isSame(s.left,t.left) && isSame(s.right,t.right);
+        return isSame(s.left, t.left) && isSame(s.right, t.right);
     }
 }
