522bd5346963cbb18df79d11dbbf945f6ee27ad6
jfson <sunzhenandroid@126.com>
2019-05-07 08:17:24 +0800

    1.quickSort

--- LeetCode/app/src/main/java/com/sun/leetcode/company/Ms/quicksort.java
+++ LeetCode/app/src/main/java/com/sun/leetcode/company/Ms/quicksort.java
@@ -8,43 +8,5 @@ package com.sun.leetcode.company.Ms;
  * Train of thought:
  */
 public class quicksort {
-    //[2,5,1,3,5,6]
 
-
-    /**
-     最坏情况，可以是=相邻的俩个数进行了交换，最差时间复杂度和冒泡排序一样是O(N^2)
-     平均时间复杂度为O(NlogN)
-     */
-
-    private void sort(int[] nums,int left,int right){
-        if (left > right) return;
-
-        int temp = nums[left];
-        int i = left;
-        int j = right;
-
-        while (i != j){
-             //顺序很重要，要从右边开始向左找，找比基准数小的
-            while (nums[j] >= temp && i < j){
-                j--;
-            }
-
-            while (nums[i] <= temp && i < j){
-                i++;
-            }
-
-            if (i < j){
-                int t = nums[i];
-                nums[i] = nums[j];
-                nums[j] = t;
-            }
-        }
-
-//        此时 i = j
-        nums[left] = nums[i];
-        nums[i] = temp;
-
-        sort(nums,left,i-1);
-        sort(nums,i+1,right);
-    }
 }
