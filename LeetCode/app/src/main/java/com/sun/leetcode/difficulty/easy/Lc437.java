9195fed8eb1a1795865fc065c7c446e923ea77a1
jfson <sunzhenandroid@126.com>
2019-05-13 23:24:06 +0800

    1.Lc437

--- LeetCode/app/src/main/java/com/sun/leetcode/difficulty/easy/Lc437.java
+++ LeetCode/app/src/main/java/com/sun/leetcode/difficulty/easy/Lc437.java
@@ -44,24 +44,19 @@ public class Lc437 {
      solution: 递归
      */
     public int pathSum(TreeNode root, int sum) {
-        if(root == null){
-            return 0;
+        if(root == null)
+                return 0;
+            return findPath(root, sum) + pathSum(root.left, sum) + pathSum(root.right, sum);
         }
 
-        return helper(root,sum) +helper(root.left,sum) + helper(root.right,sum);
-    }
-
-    public int helper(TreeNode root,int sum){
-        if(root == null){
-            return 0;
+        public int findPath(TreeNode root, int sum){
+            int res = 0;
+            if(root == null)
+                return res;
+            if(sum == root.val)
+                res++;
+            res += findPath(root.left, sum - root.val);
+            res += findPath(root.right, sum - root.val);
+            return res;
         }
-
-        int curr = 0;
-        if(root.val == sum){
-            curr += 1;
-        }
-        curr += helper(root.left,sum-root.val);
-        curr += helper(root.right,sum-root.val);
-        return curr;
-    }
 }
