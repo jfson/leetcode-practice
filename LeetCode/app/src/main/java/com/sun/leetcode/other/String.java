f0d2cf653f623a0d0f22627f88d5518cb707aa50
jfson <sunzhenandroid@126.com>
2020-03-12 23:06:31 +0800

    1.string

---
+++ LeetCode/app/src/main/java/com/sun/leetcode/other/String.java
@@ -0,0 +1,14 @@
+package com.sun.leetcode.other;
+
+/**
+ * Author: jfson sun
+ * Create on:  2020/3/10
+ * Question:
+ * Description:
+ * Train of thought:
+ */
+public class String {
+    //自定一个 java.lang.String类？会有什么问题?
+    //不可以。编译不过
+    //通过dex加载成功了也会报错。
+}
